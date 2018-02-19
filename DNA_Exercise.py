edb640729c05d6c9ea315e4f6b6721c02c1fd2d8
jjpangilinan2014 <36615736+jjpangilinan2014@users.noreply.github.com>
2018-02-19 14:23:17 +0800

    Add files via upload

---
+++ DNA_Exercise.py
@@ -0,0 +1,18 @@
+countC = 0
+countG = 0
+DNA = open("DNA.txt").read();
+totalLength = len(DNA)
+
+
+for i in range(len(DNA)):
+    if DNA[i] == "C":
+        countC = countC + 1
+
+    if DNA[i] == "G":
+        countG = countG + 1
+
+print("Number of C: ", countC)
+print("Number of G: ", countG)
+print("Total number of C+G: ", countC+countG)
+print("Total Length of DNA: ", len(DNA))
+print("Percentage: ", ((countC+countG)/totalLength)*100)
